0cd442ae69f5f41d28c4fe19461cd0bf97f718fc
Ramazon Tursunov <288622109+ramazontursunov07@users.noreply.github.com>
2026-08-08 10:54:41 +0500

    Update urls.py

--- onlineshopapp/urls.py
+++ onlineshopapp/urls.py
@@ -7,7 +7,7 @@ from onlineshopapp.admin_views import (AdminCategoryViewSet,AdminProductViewSet,
                                        admin_list_product_view_history)
 from onlineshopapp.shop_views import (CategoryViewSet,ProductViewSet,get_product_by_category_id,
                                       CartViewSet,ProfileViewSet,LogoutAPIView,WishlistViewSet,
-                                      ActiveFlashSaleViewSet,RegisterAPIView,my_orders)
+                                      ActiveFlashSaleViewSet,RegisterAPIView,support_chat)
 from .shop_views import MyTokenObtainPairView
 from rest_framework_simplejwt.views import TokenRefreshView
 
@@ -24,8 +24,8 @@ router.register(r'categories',CategoryViewSet,basename='categories')
 router.register(r'products',ProductViewSet,basename='products')
 router.register(r'cart',CartViewSet,basename='cart')
 router.register(r'profile',ProfileViewSet,basename='Profile')
-router.register(r'wishes',WishlistViewSet,basename='wishes')
-router.register(r'flash-sale',ActiveFlashSaleViewSet,basename='flash-sale')
+router.register(r'wishes',WishlistViewSet,basename='wishlist')
+router.register(r'flash-sale',ActiveFlashSaleViewSet,basename='Flashsale')
 
 
 urlpatterns = [
@@ -36,8 +36,7 @@ urlpatterns = [
     path('product-category-id/<int:category_id>/',get_product_by_category_id,name='get_product_by_category_id'),
     path('logout/',LogoutAPIView.as_view(),name='logout'),
     path('register/',RegisterAPIView.as_view(),name='register'),
+    path('support-chat/', support_chat, name='support-chat'),
     path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('orders/',my_orders,name='my-orders')
-
 ]
